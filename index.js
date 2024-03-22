6c94a0c771b8e6c135d3ffafeb3165b5bc8f6c5b
Jesús de Israel Jiménez Tostado <israeljimenezcontacto@gmail.com>
2024-03-22 13:12:05 -0700

    Path * agregado

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ app.use(express.static('public'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
 
 // Escuchar peticiones
 app.listen(process.env.PORT, () => {
